f1ee1f312b955ff532255685c01cc0579a4411be
AzharMehmood4 <azharzharmehmoodm@gmail.com>
2026-02-09 20:37:14 +0500

    view

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import View from "./components/View";
 export default function App() {
   return (
     <Router>
@@ -8,6 +9,7 @@ export default function App() {
 
       <Routes>
         <Route path="/" element={<Login />} />
+         <Route path="/view" element={<View />} />
       </Routes>
     </Router>
   );
